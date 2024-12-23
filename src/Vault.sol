0ed50ccf8d70fa72eef86e7c174c26f69fff6324
ququzone <xueping.yang@gmail.com>
2024-12-23 12:43:50 +0800

    feat: implement Vault

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,14 +1,50 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
 
-contract Counter {
-    uint256 public number;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Vault is Ownable, ReentrancyGuard {
+    using Address for address payable;
+
+    error ExistsClaimer(address account);
+    error NonClaimer(address account);
+
+    event AddClaimer(address indexed claimer);
+    event RemoveClaimer(address indexed claimer);
+    event Claim(address indexed claimer, address indexed account, uint256 amount);
+
+    mapping(address => bool) public claimer;
+
+    constructor() Ownable(msg.sender) {}
+
+    function addClaimer(address _claimer) external onlyOwner {
+        if (claimer[_claimer]) {
+            revert ExistsClaimer(_claimer);
+        }
+
+        claimer[_claimer] = true;
+        emit AddClaimer(_claimer);
     }
 
-    function increment() public {
-        number++;
+    function removeClaimer(address _claimer) external onlyOwner {
+        if (!claimer[_claimer]) {
+            revert NonClaimer(_claimer);
+        }
+
+        claimer[_claimer] = false;
+        emit RemoveClaimer(_claimer);
     }
+
+    function claim(address _recipient, uint256 _amount) external nonReentrant {
+        if (!claimer[msg.sender]) {
+            revert NonClaimer(msg.sender);
+        }
+
+        payable(_recipient).sendValue(_amount);
+        emit Claim(msg.sender, _recipient, _amount);
+    }
+
+    receive() external payable {}
 }
